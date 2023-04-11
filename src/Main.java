1db545ec1a5b0e51b5589edaab97d08e06d41954
hardeek-simform <hardeek.b@simformsolutions.com>
2023-04-11 14:20:07 +0530

    Completed the demo

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import com.student.record.StudentDetails;
+
+public class Main extends StudentDetails{
+    public static void main(String[] args) {
+        Main childObj = new Main();
+        System.out.println("Welcome to student portal!!!");
+        System.out.println("Your current details are:");
+        System.out.println("Roll number:" + childObj.rollNo);
+        System.out.println("Standard:" + childObj.standard);
+        System.out.println("Phone Number:" + childObj.getPhoneNumber());
+
+        childObj.rollNo = 12;
+        childObj.standard = 11;
+        childObj.setPhoneNumber("9123123123");
+
+        System.out.println("---------------------------------------------------");
+        System.out.println("As per your request we have updated the details:");
+        System.out.println("Roll number:" + childObj.rollNo);
+        System.out.println("Standard:" + childObj.standard);
+        System.out.println("Phone Number:" + childObj.getPhoneNumber());
+
+    }
+}
---
+++ src/com/student/record/StudentDetails.java
@@ -0,0 +1,15 @@
+package com.student.record;
+
+public class StudentDetails {
+    private String phoneNumber = "9876543219";
+    protected int standard = 10;
+    public int rollNo = 10;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
